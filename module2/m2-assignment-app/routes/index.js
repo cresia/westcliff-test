027f51a5bf2a6a333b4b0a23f0fcf30c4a857cfc
cresia <kimkim4@gmail.com>
2021-03-06 18:40:47 -0800

    complete m3-assignment

--- module2/m2-assignment-app/routes/index.js
+++ module2/m2-assignment-app/routes/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 router.get('/', function(req, res) {
   // res.send('It works!');
   // res.render('form');
   res.render('form', {title: 'Registration form'});
+});
 
-})
+router.post('/', [
+  check('name')
+    .isLength({ main: 1})
+    .withMessage('Please enter a name'),
+  check('email')
+    .isLength({ main: 1 })
+    .withMessage('Please enter an email'),
+],
+function(req, res) {
+  // console.log(req.body);
+  const errors = validationResult(req);
+  if(errors.isEmpty()) {
+    res.send('Thank you for your registration!');
+  } else {
+    res.render('form', {
+      title: "Registration form",
+      errors: errors.array(),
+      data: req.body,
+    });
+  }
+  res.render('form', {title: 'Registration form'});
+});
 
 module.exports = router;
