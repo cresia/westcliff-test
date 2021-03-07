e8ad5477a07b9b869c3beda62c5463fda741137a
cresia <kimkim4@gmail.com>
2021-03-06 17:39:14 -0800

    module3 assignment

---
+++ module2/m2-assignment-app/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  // res.send('It works!');
+  // res.render('form');
+  res.render('form', {title: 'Registration form'});
+
+})
+
+module.exports = router;
