3b8d80859355663887bc4b13550df4dee683691f
cresia <kimkim4@gmail.com>
2021-03-09 11:44:06 -0800

    still working

---
+++ back-end/wk1-day2-homework/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+const { listen } = require('app');
+
+var app = express();
+
+// app.use(express.static(path.join('public')));
+// app.use(express.static(path.join('public/html')));
+app.use(express.static(path.join('html')));
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
