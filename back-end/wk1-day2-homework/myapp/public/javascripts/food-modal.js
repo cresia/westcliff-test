2996bdd0f753d4c3afb22faa5601992df74fbf86
cresia <kimkim4@gmail.com>
2021-03-09 16:21:08 -0800

    Complete hw2 module3

--- back-end/wk1-day2-homework/myapp/public/javascripts/food-modal.js
+++ back-end/wk1-day2-homework/myapp/public/javascripts/food-modal.js
@@ -5,26 +5,26 @@ function showDescription(){
     if(temp == "img1")
     {
         document.getElementById("title").innerText = this.obj1.author;
-        document.getElementById("foodieLevel").innerText = this.obj1.level;
-        document.getElementById("biography").innerText = this.obj1.bio;
+        document.getElementById("foodLevel").innerText = this.obj1.level;
+        document.getElementById("bio").innerText = this.obj1.bio;
     }
     else if(temp == "img2")
     {
         document.getElementById("title").innerText = this.obj2.author;
-        document.getElementById("foodieLevel").innerText = this.obj2.level;
-        document.getElementById("biography").innerText = this.obj2.bio;
+        document.getElementById("foodLevel").innerText = this.obj2.level;
+        document.getElementById("bio").innerText = this.obj2.bio;
     }
     else if(temp == "img3")
     {
         document.getElementById("title").innerText = this.obj3.author;
-        document.getElementById("foodieLevel").innerText = this.obj3.level;
-        document.getElementById("biography").innerText = this.obj3.bio;
+        document.getElementById("foodLevel").innerText = this.obj3.level;
+        document.getElementById("biog").innerText = this.obj3.bio;
     }
     else
     {
         document.getElementById("title").innerText = this.obj4.author;
-        document.getElementById("foodieLevel").innerText = this.obj4.level;
-        document.getElementById("biography").innerText = this.obj4.bio;
+        document.getElementById("foodLevel").innerText = this.obj4.level;
+        document.getElementById("bio").innerText = this.obj4.bio;
     }
 }
 
