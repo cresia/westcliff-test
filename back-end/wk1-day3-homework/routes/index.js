2694eeeb11cd433eb3363fd6356f7bd373902653
cresia <kimkim4@gmail.com>
2021-03-09 18:04:30 -0800

    module3 hw3

--- back-end/wk1-day3-homework/routes/index.js
+++ back-end/wk1-day3-homework/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  // res.render('form', { title: 'Registration form' });
+  res.render('foodblog', { title: 'Food Blog' });
 });
+
 
 module.exports = router;
