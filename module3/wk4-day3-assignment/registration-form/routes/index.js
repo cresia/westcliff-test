662dd0bd20b39c2062fc16eec853e87c2aac882d
cresia <kimkim4@gmail.com>
2021-04-10 14:44:52 -0700

    Complete a continous lab from wk4-day3-assignment for wk6-day3 lab

--- module3/wk4-day3-assignment/registration-form/routes/index.js
+++ module3/wk4-day3-assignment/registration-form/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const auth = require('http-auth');
+const bcrypt = require('bcrypt');
+
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
@@ -26,12 +28,12 @@ router.get('/registrants', basic.check((req, res) => {
     .then((registrations) => {
       res.render('registrants', { title: 'Listing registrations', registrations });
     })
-    .catch(() => { 
-      res.send('Sorry! Something went wrong.'); 
+    .catch(() => {
+      res.send('Sorry! Something went wrong.');
     });
 }));
 
-router.post('/', 
+router.post('/',
     [
         check('name')
         .isLength({ min: 1 })
@@ -39,12 +41,24 @@ router.post('/',
         check('email')
         .isLength({ min: 1 })
         .withMessage('!Error: Please enter an email'),
+        check('username')
+        .isLength({ min: 1 })
+        .withMessage('!Error: Please enter username'),
+        check('password')
+        .isLength({ min: 1 })
+        .withMessage('!Error: Please enter password'),
     ],
-    (req, res) => {
+    async (req, res) => {
         //console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
+          // console.log(req.body);
           const registration = new Registration(req.body);
+          // console.log(registration)
+          //generate salt to hash password
+          const salt = await bcrypt.genSalt(10);
+          //set user password to hashed password
+          registration.password = await bcrypt.hash(registration.password, salt);
           registration.save()
             .then(() => {res.render('thankyou', {title:'Thankyou form'});})
             .catch((err) => {
@@ -52,7 +66,7 @@ router.post('/',
               res.send('Sorry! Something went wrong.');
             });
           } else {
-            res.render('register', { 
+            res.render('register', {
                 title: 'Register form',
                 errors: errors.array(),
                 data: req.body,
