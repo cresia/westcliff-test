78659a789e47f58b79496a2fadc08f7aae5dabc5
cresia <kimkim4@gmail.com>
2021-06-22 16:45:23 -0700

    finish hw

--- Module4-(React)/Homework/m6-w3-d2-homework/router-params/src/App.js
+++ Module4-(React)/Homework/m6-w3-d2-homework/router-params/src/App.js
@@ -11,22 +11,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 export default function App() {
-  //Insert router, links here
-  <BrowserRouter>
-    <Switch>
-      <Route path="/https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" />
-      <Route path="/https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg" />
-      <Route path="/https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg" />
-      <Route path="/https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png" />
-    </Switch>
-  </BrowserRouter>
-
+  return (
+    <Router>
+      <h1>TV APPS</h1>
+      <nav className="justify-content-center" style={{ margin: 50 }}>
+        <Link to="/netflix" style={{ padding: 5 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" alt="Netflix Logo" />
+        </Link>
+        <Link to="/hbomax" style={{ padding: 5 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg" alt="HBOMax Logo" />
+        </Link>
+        <Link to="/hulu" style={{ padding: 5 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg" alt="HULU Logo" />
+        </Link>
+        <Link to="/prime" style={{ padding: 5 }}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png" alt="Prime Video Logo" />
+        </Link>
+      </nav>
+      <Switch>
+        <Route exact path="/:id">
+          <Child />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 function Child() {
-
-  // Below this comment, there's one major key script missing
-
+  let params = useParams();
+  let id = params.id;
+  console.log(id);
 
   return (
     <div>
